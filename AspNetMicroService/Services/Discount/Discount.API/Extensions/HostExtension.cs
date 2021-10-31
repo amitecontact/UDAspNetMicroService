58e3a815d4f409b74eadb1ae8ea214c443c7b0ba
Amit Ingole <amitecontact@gmail.com>
2021-10-31 19:05:24 +0530

    Added discount grpc services project

---
+++ AspNetMicroService/Services/Discount/Discount.API/Extensions/HostExtension.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Discount.API.Extensions
+{
+    public static class HostExtension
+    {
+        public static IHost MigrateDatabase<TContext>(this IHost host, int? retryCount = 0)
+        {
+            int retryForAvailability = retryCount.Value;
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var serviceProvider = scope.ServiceProvider;
+                var config = serviceProvider.GetRequiredService<IConfiguration>();
+                var logger = serviceProvider.GetRequiredService<ILogger<TContext>>();
+
+                logger.LogInformation("Migrating postresql database.");
+
+                try
+                {
+                    using var connection = new NpgsqlConnection
+                       (config.GetValue<string>("DatabaseSettings:ConnectionString"));
+                    connection.Open();
+
+                    using var command = new NpgsqlCommand()
+                    {
+                        Connection = connection
+                    };
+
+                    command.CommandText = "DROP TABLE IF EXISTS Coupon";
+                    command.ExecuteNonQuery();
+
+                    command.CommandText = @"CREATE TABLE Coupon(Id SERIAL PRIMARY KEY, 
+                                                                ProductName VARCHAR(24) NOT NULL,
+                                                                Description TEXT,
+                                                                Amount INT)";
+                    command.ExecuteNonQuery();
+
+                    command.CommandText = "INSERT INTO Coupon(ProductName, Description, Amount) VALUES('IPhone X', 'IPhone Discount', 150);";
+                    command.ExecuteNonQuery();
+
+                    command.CommandText = "INSERT INTO Coupon(ProductName, Description, Amount) VALUES('Samsung 10', 'Samsung Discount', 100);";
+                    command.ExecuteNonQuery();
+
+                    logger.LogInformation("Migrated postresql database.");
+
+                }
+                catch (NpgsqlException ex)
+                {
+
+                    logger.LogError(ex, "Error while trying to access PG SQL Instance {0}", retryForAvailability);
+
+                    if (retryForAvailability < 50)
+                    {
+                        retryForAvailability++;
+                        System.Threading.Thread.Sleep(2000);
+                        MigrateDatabase<IHost>(host, retryForAvailability);
+                    }
+                }
+            }
+
+            return host;
+        }
+    }
+}
