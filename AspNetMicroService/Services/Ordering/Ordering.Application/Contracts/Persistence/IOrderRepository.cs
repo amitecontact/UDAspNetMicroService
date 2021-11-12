6de3e62946eef7e2e1b91728a9bd3d55e81eff85
Amit Ingole <amitecontact@gmail.com>
2021-11-12 14:11:55 +0530

    Added Ordering Project wrt clean architecture Added dependencies Added  Contracts of Application Layers

---
+++ AspNetMicroService/Services/Ordering/Ordering.Application/Contracts/Persistence/IOrderRepository.cs
@@ -0,0 +1,14 @@
+﻿using Ordering.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Contracts.Persistence
+{
+    interface IOrderRepository : IAsynRepository<Order>
+    {
+        Task<IReadOnlyList<Order>> GetOrderByUserName(string userName);
+    }
+}
